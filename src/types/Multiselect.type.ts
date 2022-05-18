2e8926243e4887bd0440ba47fd4616f821cd330c
Merlin Flach <merlin.flach@singular-it.de>
2022-05-18 15:51:45 +0200

    add index for export and type

---
+++ src/types/Multiselect.type.ts
@@ -0,0 +1,29 @@
+import {VNode} from 'vue';
+import Option from "./option.type";
+
+declare class Multiselect {
+    modelValue?: any;
+    selectOptions: any;
+    placeholder?: string;
+    multipleLabel?: string | Function;
+    canClear?: boolean;
+    disabled?: boolean;
+    searchable?: boolean;
+
+    $emit(eventName: 'select', e: { originalEvent: Event,  option: Option }): this;
+    $emit(eventName: 'deselect', e: { originalEvent: Event, option: Option }): this;
+    $emit(eventName: 'search-change', e: { originalEvent: Event, query: string }): this;
+    $emit(eventName: 'open'): this;
+    $emit(eventName: 'close'): this;
+    $emit(eventName: 'clear'): this;
+
+    $slots: {
+        placeholder: VNode[];
+        multipleLabel: VNode[];
+        optionLabel: VNode[];
+        clear: VNode[];
+    }
+
+}
+
+export default Multiselect
