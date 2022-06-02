bb5aaf66e786dda287316f1b418716d42f80d72d
Merlin Flach <merlin.flach@singular-it.de>
2022-06-02 11:11:32 +0200

    add test component and tests

---
+++ src/tests/MultiselectTester.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="mt-3 mx-5 flex flex-col">
+    <Multiselect
+        v-model="selected"
+        :canClear="canClear"
+        :close-on-select="closeOnSelect"
+        :disabled="disabled"
+        :input-type="inputType"
+        :multiple="multiple"
+        :multiple-label="multipleLabel"
+        :placeholder="placeholder"
+        :searchable="searchable"
+        :select-options="selectOptions"
+    />
+    <div v-if="vModel">
+      <button class="mt-4 text-center w-full border-2 bg-gray-100" data-cy="pushButton" @click="pushValue">Push a random
+        value from Options
+      </button>
+      <button class="mt-4 text-center w-full border-2 bg-gray-100" data-cy="illegalPushButton"
+              @click="pushIllegalValue">Push an illegal Value
+      </button>
+    </div>
+    <div v-if="vModel" class="mt-4">
+      <span>selected values:<br></span>
+      <ul v-if="multiple">
+        <li v-for="(select, id) in selected" :key="id" data-cy="selected">{{ select }}</li>
+      </ul>
+      <span v-else data-cy="selected">{{ selected }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref, toRefs} from "vue";
+import Multiselect from "../Multiselect.vue";
+import Option from "../types/option";
+
+export default defineComponent({
+  name: 'MultiselectTester',
+  components: {Multiselect},
+  props: {
+    vModel: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    multiple: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    multipleLabel: {
+      type: [Function, String],
+      required: false,
+    },
+    selectOptions: {
+      type: Array,
+      required: false,
+      default: () => ([])
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+    canClear: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    searchable: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    inputType: {
+      type: String,
+      required: false,
+      default: 'text',
+    },
+    closeOnSelect: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  setup(props) {
+    const {vModel, multiple, selectOptions} = toRefs(props)
+    let selected: any
+
+    if (vModel.value) {
+      if (multiple.value) {
+        selected = ref([])
+      } else {
+        selected = ref()
+      }
+    }
+
+    function pushValue() {
+      if (vModel.value) {
+        const value = (selectOptions.value[Math.floor(Math.random() * selectOptions.value.length)] as Option).value
+        if (multiple.value) {
+          selected.value.push(value)
+        } else {
+          selected.value = value
+        }
+      }
+    }
+
+    function pushIllegalValue() {
+      if (vModel.value) {
+        const value = {this: 'Hallo', is: 1234, a: {Test: 'okayyy, let\'s go'}}
+        if (multiple.value) {
+          selected.value.push(value)
+        } else {
+          selected.value = value
+        }
+      }
+    }
+
+    return {
+      selected,
+      pushValue,
+      pushIllegalValue
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/tests/multiselect.spec.js
@@ -0,0 +1,158 @@
+import {mount} from '@cypress/vue'
+import MultiselectTester from "./MultiselectTester.vue"
+import '../index.css'
+
+describe('Multiselect Component', () => {
+    it('Can be mounted', () => {
+        mount(MultiselectTester)
+        cy.get('[data-cy="multiselect"]').should('exist')
+    })
+
+    it('Can have a placeholder text', () => {
+        mount(MultiselectTester, {
+            props: {
+                placeholder: 'test',
+            }
+        })
+        cy.get('.multiselect-placeholder').should('be.visible')
+    })
+
+    it('Works as select input for single selections', () => {
+        const selectOptions = [{value: {abc: 'xyz', test: {xyz: 3}}, label: 'This'}, {value: 2, label: 'is'}, {value: 'test', label: 'a'}, {value: 4, label: 'test'}]
+
+        mount(MultiselectTester, {
+            props: {
+                vModel: true,
+                selectOptions: selectOptions,
+            },
+        })
+
+        cy.get('[data-cy="multiselect"]').should('exist')
+        cy.get('[data-cy="dropdown"]').should('have.class', 'is-hidden')
+        cy.get('[data-cy="multiselect"]').click()
+        cy.get('[data-cy="dropdown"]').should('not.have.class', 'is-hidden')
+        cy.get('[data-cy="option"]').eq(3).click()
+        cy.get('[data-cy="label"]').contains('test')
+        cy.get('[data-cy="selected"]').contains('4')
+        cy.get('[data-cy="option"]').eq(0).click()
+        cy.get('[data-cy="label"]').contains('This')
+        cy.get('[data-cy="label"]').not('test')
+        cy.get('[data-cy="selected"]').contains('{ \"abc\": \"xyz\", \"test\": { \"xyz\": 3 } }')
+        cy.get('[data-cy="selected"]').not('4')
+        cy.get('[data-cy="clear"]').click()
+        cy.get('[data-cy="label"]').should('not.exist')
+        cy.get('[data-cy="dropdown"]').should('have.class', 'is-hidden')
+        cy.get('[data-cy="pushButton"]').click()
+        cy.get('[data-cy="label"]').should('exist')
+        cy.get('[data-cy="selected"]').should('exist')
+        cy.get('[data-cy="clear"]').trigger('mousedown')
+        cy.get('[data-cy="illegalPushButton"]').click()
+        cy.get('[data-cy="label"]').should('not.exist')
+        cy.get('[data-cy="selected"]').should('be.empty')
+        cy.get('[data-cy="multiselect"]').click()
+        cy.get('[data-cy="option"]').eq(0).click()
+        cy.get('[data-cy="multiselect"]').trigger('focusout')
+        cy.get('[data-cy="illegalPushButton"]').click()
+        cy.get('[data-cy="label"]').should('not.exist')
+        cy.get('[data-cy="selected"]').should('be.empty')
+
+    })
+
+    it('Works as select input for multiple selections', () => {
+        const selectOptions = [{value: {abc: 'xyz', test: {xyz: 3}}, label: 'This'}, {value: 2, label: 'is'}, {value: 'test', label: 'a'}, {value: 4, label: 'test'}]
+        mount(MultiselectTester, {
+            props: {
+                vModel: true,
+                selectOptions: selectOptions,
+                multiple: true
+            },
+        })
+
+        cy.get('[data-cy="dropdown"]').should('have.class', 'is-hidden')
+        cy.get('[data-cy="multiselect"]').click()
+        cy.get('[data-cy="dropdown"]').should('not.have.class', 'is-hidden')
+        cy.get('[data-cy="option"]').eq(3).click()
+        cy.get('[data-cy="label"]').contains('1 Option gewählt')
+        cy.get('[data-cy="selected"]').contains(4)
+        cy.get('[data-cy="option"]').eq(2).click()
+        cy.get('[data-cy="label"]').contains('2 Optionen gewählt')
+        cy.get('[data-cy="selected"]').contains(4)
+        cy.get('[data-cy="selected"]').contains('test')
+        cy.get('[data-cy="option"]').eq(0).click()
+        cy.get('[data-cy="label"]').contains('3 Optionen gewählt')
+        cy.get('[data-cy="selected"]').contains(4)
+        cy.get('[data-cy="selected"]').contains('test')
+        cy.get('[data-cy="selected"]').contains('{ "abc": "xyz", "test": { "xyz": 3 } }')
+        cy.get('[data-cy="option"]').eq(1).click()
+        cy.get('[data-cy="label"]').contains('4 Optionen gewählt')
+        cy.get('[data-cy="selected"]').contains(4)
+        cy.get('[data-cy="selected"]').contains('test')
+        cy.get('[data-cy="selected"]').contains('{ "abc": "xyz", "test": { "xyz": 3 } }')
+        cy.get('[data-cy="selected"]').contains(2)
+        cy.get('[data-cy="option"]').eq(2).click()
+        cy.get('[data-cy="label"]').contains('3 Optionen gewählt')
+        cy.get('[data-cy="selected"]').not('test')
+        cy.get('[data-cy="clear"]').click()
+        cy.get('[data-cy="label"]').should('not.exist')
+        cy.get('[data-cy="dropdown"]').should('have.class', 'is-hidden')
+        cy.get('[data-cy="pushButton"]').click()
+        cy.get('[data-cy="label"]').should('exist')
+        cy.get('[data-cy="label"]').contains('1 Option gewählt')
+        cy.get('[data-cy="selected"]').should('exist')
+        cy.get('[data-cy="selected"]').should('not.be.empty')
+        cy.get('[data-cy="illegalPushButton"]').click()
+        cy.get('[data-cy="selected"]').should('have.length', 1)
+        cy.get('[data-cy="label"]').contains('1 Option gewählt')
+    })
+
+    it('Can close the dropdown on selection', ()=>{
+        const selectOptions = [{value: {abc: 'xyz', test: {xyz: 3}}, label: 'This'}, {value: 2, label: 'is'}, {value: 'test', label: 'a'}, {value: 4, label: 'test'}]
+        mount(MultiselectTester, {
+            props: {
+                selectOptions: selectOptions,
+                closeOnSelect: true
+            },
+        })
+
+        cy.get('[data-cy="dropdown"]').should('have.class', 'is-hidden')
+        cy.get('[data-cy="multiselect"]').click()
+        cy.get('[data-cy="dropdown"]').should('not.have.class', 'is-hidden')
+        cy.get('[data-cy="option"]').eq(3).click()
+        cy.get('[data-cy="dropdown"]').should('have.class', 'is-hidden')
+    })
+
+    it('Is can have an searchable input', ()=>{
+        const selectOptions = [{value: {abc: 'xyz', test: {xyz: 3}}, label: 'This'}, {value: 2, label: 'is'}, {value: 'test', label: 'a'}, {value: 4, label: 'test'}]
+        mount(MultiselectTester, {
+            props: {
+                selectOptions: selectOptions,
+                searchable: true
+            },
+        })
+
+        cy.get('[data-cy="dropdown"]').should('have.class', 'is-hidden')
+        cy.get('[data-cy="multiselect"]').click()
+        cy.get('[data-cy="dropdown"]').should('not.have.class', 'is-hidden')
+        cy.get('[data-cy="searchInput"').should('be.focused')
+        cy.get('[data-cy="searchInput"').type('test')
+        cy.get('[data-cy="dropdown"]').contains('test')
+        cy.get('[data-cy="dropdown"]').not('this')
+        cy.get('[data-cy="dropdown"]').not('is')
+        cy.get('[data-cy="dropdown"]').not('a')
+    })
+
+    it('Can be disabled', ()=> {
+        const selectOptions = [{value: {abc: 'xyz', test: {xyz: 3}}, label: 'This'}, {value: 2, label: 'is'}, {value: 'test', label: 'a'}, {value: 4, label: 'test'}]
+        mount(MultiselectTester, {
+            props: {
+                selectOptions: selectOptions,
+                disabled: true
+            },
+        })
+
+        cy.get('[data-cy="dropdown"]').should('have.class', 'is-hidden')
+        cy.get('[data-cy="multiselect"]').click()
+        cy.get('[data-cy="dropdown"]').should('have.class', 'is-hidden')
+
+    })
+})
