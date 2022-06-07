99fdb7c613bcb7e1f9541ffb82304b1e717897b7
Merlin Flach <merlin.flach@singular-it.de>
2022-06-07 18:07:14 +0200

    add storybook

---
+++ src/stories/Multiselect.stories.js
@@ -0,0 +1,21 @@
+import Multiselect from '../Multiselect.vue';
+import '../index.css';
+
+export default {
+    title: 'Component/Multiselect',
+    component: Multiselect,
+}
+
+const Template = (args) => ({
+    components: {Multiselect},
+    setup() {
+        return {args};
+    },
+    template: '<multiselect v-bind="args"/>'
+})
+
+export const MultiselectComponent = Template.bind({});
+MultiselectComponent.args={
+    placeholder: 'huhu',
+    selectOptions: [{value: 1, label: 'Wie'},{value: 'hallo', label: 'geht'},{value: {nested: 'juhu'}, label: 'es'},{value: true, label: 'dir'}]
+}
