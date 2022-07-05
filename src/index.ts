58b7894aa6ef621e1ab4e906fb7e08a4092e90d9
Merlin Flach <merlin.flach@singular-it.de>
2022-07-05 18:03:04 +0200

    feature: add types to build

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export {default} from './Multiselect.vue'
 export {baseStyle} from "./utils/defaultTheme"
+
+export type {Option as SMultiselectOption, Classes as SMultiselectClasses} from './types'
+
+export * from './utils'
---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+export {defaultTheme as SMultiselectDefaultTheme} from './defaultTheme'
+export {baseStyle as SMultiselectBaseStyle} from './defaultTheme'
+
+export * from './useClasses'
+export * from './useDropdown'
+export * from './useMultiselect'
+export * from './useOptions'
+export * from './useSearch'
+export * from './useValue'
