e4502bb7f1c45568a3875a8e8788b8e7023276e1
Robin Kehl <robin.kehl@singular-it.de>
2022-07-15 18:50:17 +0200

    test docs

---
+++ docs/assets/config.8209250c.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory.a6813538.js";import"./iframe.94971474.js";import"./vue.esm-bundler.fdd7e61a.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config.8209250c.js.map
