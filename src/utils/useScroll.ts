6f923bd327264eae043c28ad8c5c5d8faa32c0da
Merlin Flach <merlin.flach@singular-it.de>
2022-11-07 16:19:29 +0100

    fix: loadMore is only emitted once

--- src/utils/useScroll.ts
+++ src/utils/useScroll.ts
@@ -1,6 +1,7 @@
 import type {Ref, SetupContext} from 'vue'
 import {computed} from 'vue'
 import type {Option} from '../types'
+import _ from "lodash";
 
 export default function useScroll(
   infinite: Ref<boolean>,
@@ -12,11 +13,11 @@ export default function useScroll(
     return selectOptions.value.length < maxOptions.value
   })
 
-  function handleScroll(e: Event) {
+  const handleScroll = _.debounce((e: Event) => {
     const {scrollTop, offsetHeight, scrollHeight} = e.target as HTMLDivElement
     if (scrollTop + offsetHeight >= scrollHeight && hasMore.value)
       context.emit('loadMore')
-  }
+  }, 50)
 
   return {
     hasMore,
