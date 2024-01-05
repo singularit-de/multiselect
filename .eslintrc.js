955944a10aff2efdcb45f18c2f4e09b2f56dc0a3
Merlin <merlin.flach@singular-it.de>
2024-01-05 15:02:04 +0100

    feat: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: '@singularit',
+  extends: ['@singularit/eslint-config-vue', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
